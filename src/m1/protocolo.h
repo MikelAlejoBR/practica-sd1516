7d789e8c0e04aca179ff1f65dfff0ebb3fd066ff
MikelAlejoBR <mikelalejo2155@gmail.com>
2016-01-10 18:26:11 +0100

    Add missing sendFile function declaration

--- src/m1/protocolo.h
+++ src/m1/protocolo.h
@@ -1,9 +1,11 @@
-#define PORT 50000
+#define PORT 6012
 #define TIMEOUT 10;
 
 int waitforack(int sock);
+int sendConnect(int sock);
 int sendack(int sock);
 int senderr(int sock, int ecode);
+int sendFile(int sock, char * file);
 int sendfin(int sock);
 int getsockfd();
 int releasesockfd(int sock);
