8b132d3f4e6b1b81a45da0587417f9851050646a
JoseJavierRR <jose.ehu.javier@gmail.com>
2016-01-10 16:05:50 +0100

    Error resuelto
    Faltaba actualizar my_dropbox_server.h

--- src/m2/my_dropbox_server.h
+++ src/m2/my_dropbox_server.h
@@ -1,16 +1,13 @@
 #define MAX_BUF 1024
 #define PORT 6012
-
-#define ST_INIT	0
+#define FILENAME  "./MiNube/"
 
 #define COM_TRF	1
 #define COM_DEL	2
-#define COM_FIN	5
+#define COM_REN	4
 
-char * comandos[]  = {"CON","TRF", "DEL", "ACK", "ERR", "FIN", NULL};
+char * comandos[]  = {"CON","TRF", "DEL", "ACK", "REN", "FIN", NULL};
 
-int estado;
-
-void sesion(int sock);
-int busca_string(char *string, char **strings);
+void update(int sock);
+int posc( char cad[], char c);
 int busca_substring(char *string, char **strings);
